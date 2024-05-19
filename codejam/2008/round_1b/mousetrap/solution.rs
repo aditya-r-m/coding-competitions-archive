71099a267f238a02165c5f9ba3667e2cfb32cc6c
Aditya Mishra <adityam.rtm@gmail.com>
2024-05-19 16:16:33 +0530

    simplify mousetrap solution

--- codejam/2008/round_1b/mousetrap/solution.rs
+++ codejam/2008/round_1b/mousetrap/solution.rs
@@ -1,5 +1,3 @@
-use lib::collections::{SegmentNode, SegmentTree};
-
 lib::run!();
 
 struct TestCase {
@@ -14,97 +12,68 @@ fn read() -> TestCase {
     TestCase { k, queries }
 }
 
-#[derive(Clone, Copy)]
-struct S {
-    value: usize,
-    capacity_self: usize,
-    capacity_left: usize,
+struct SegmentTree {
+    range: Vec<[usize; 2]>,
+    space: Vec<usize>,
+    value: Vec<usize>,
 }
 
-#[derive(Clone, Copy)]
-struct U {
-    index: usize,
-    value: usize,
-}
-
-fn state_initializer(min: usize, mid: usize, max: usize) -> S {
-    S {
-        value: 0,
-        capacity_self: 1 + max - min,
-        capacity_left: 1 + mid - min,
+impl SegmentTree {
+    fn new(k: usize) -> SegmentTree {
+        let mut segment_tree = SegmentTree {
+            range: vec![[0, 0]; k << 2],
+            space: vec![0; k << 2],
+            value: vec![0; k << 2],
+        };
+        segment_tree.init(0, 0, k - 1);
+        segment_tree
     }
-}
 
-fn update_propagator(node: &SegmentNode<S>, mut u: U) -> (Option<U>, Option<U>) {
-    if node.min == node.max {
-        (None, None)
-    } else if u.index < node.state.capacity_left {
-        (Some(u), None)
-    } else {
-        u.index -= node.state.capacity_left;
-        (None, Some(u))
-    }
-}
-
-fn update_aggregator(node: &SegmentNode<S>, u: U, l: Option<S>, r: Option<S>) -> S {
-    if node.min == node.max {
-        S {
-            value: u.value,
-            capacity_self: 0,
-            capacity_left: 0,
-        }
-    } else {
-        S {
-            value: 0,
-            capacity_self: l.unwrap().capacity_self + r.unwrap().capacity_self,
-            capacity_left: l.unwrap().capacity_self,
+    fn init(&mut self, i: usize, l: usize, r: usize) {
+        self.space[i] = 1 + r - l;
+        self.range[i] = [l, r];
+        if l < r {
+            self.init((i << 1) + 1, l, l + ((r - l) >> 1));
+            self.init((i + 1) << 1, 1 + l + ((r - l) >> 1), r);
         }
     }
-}
 
-fn query_propagator(node: &SegmentNode<S>, q: usize) -> (Option<usize>, Option<usize>) {
-    if node.min == node.max {
-        (None, None)
-    } else if q <= node.mid {
-        (Some(q), None)
-    } else {
-        (None, Some(q))
+    fn insert(&mut self, i: usize, mut key: usize, val: usize) {
+        self.space[i] -= 1;
+        let [l, r] = self.range[i];
+        if l == r {
+            self.value[i] = val;
+        } else if self.space[(i << 1) + 1] > key {
+            self.insert((i << 1) + 1, key, val);
+        } else {
+            key -= self.space[(i << 1) + 1];
+            self.insert((i + 1) << 1, key, val);
+        }
     }
-}
 
-fn query_aggregator(node: &SegmentNode<S>, _: usize, l: Option<usize>, r: Option<usize>) -> usize {
-    if node.min == node.max {
-        node.state.value
-    } else {
-        l.or(r).unwrap()
+    fn query(&self, i: usize, key: usize) -> usize {
+        let [l, r] = self.range[i];
+        if l == r {
+            self.value[i]
+        } else if self.range[(i << 1) + 1][1] >= key {
+            self.query((i << 1) + 1, key)
+        } else {
+            self.query((i + 1) << 1, key)
+        }
     }
 }
 
 fn solve(TestCase { k, queries }: TestCase) -> String {
-    let mut segment_tree: SegmentTree<S, U, usize, usize> = SegmentTree::create(
-        0,
-        k - 1,
-        true,
-        state_initializer,
-        update_propagator,
-        update_aggregator,
-        query_propagator,
-        query_aggregator,
-    );
-
+    let mut segment_tree = SegmentTree::new(k);
     let mut c = 0usize;
     for i in 0..k {
         c += i;
         c %= k - i;
-        segment_tree.update(U {
-            index: c,
-            value: i + 1,
-        });
+        segment_tree.insert(0, c, i + 1);
     }
-
     queries
         .into_iter()
-        .map(|i| format!("{}", segment_tree.query(i - 1)))
+        .map(|i| format!("{}", segment_tree.query(0, i - 1)))
         .collect::<Vec<String>>()
         .join(" ")
 }
